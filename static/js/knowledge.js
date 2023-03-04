01a0dc27ef74be40ffa79bade41d4cf09daaa001
Huang <2020151064@email.szu.edu.cn>
2023-03-05 03:02:47 +0800

    知识和用户管理界面的查看，搜索，删除功能

--- static/js/knowledge.js
+++ static/js/knowledge.js
@@ -11,9 +11,8 @@ function initialization(tablename) {
         pagenum = 1;
 
     var xhr = new XMLHttpRequest();
-    //user_management
-    // if (tablename == "user_management")
-    url = `http://1.12.74.230/api/user/page/${pagenum}/${pagesize}`;
+    //knowledge
+    url = `http://1.12.74.230/api/knowledge/page/${pagenum}/${pagesize}`;
     xhr.open("GET", url, true);
     xhr.send();
 
@@ -23,7 +22,7 @@ function initialization(tablename) {
             console.log(response);
 
             //初始化用户信息
-            init_user(response.data.records);
+            init_knowledge(response.data.records);
 
             //初始化进度栏   第几页， 总共几页
             init_pagelist(tablename, pagenum, response.data.pages);
@@ -49,25 +48,28 @@ function get_pagenum(tablename) {
         location.href = url;
 }
 
-//构造用户表格内容
-function init_user(res) {
+//构造知识表格内容（设置隐藏长度！！！）
+function init_knowledge(res) {
     var tbody = document.querySelector("tbody");
     tbody.innerHTML = "";
     var info = "";
     for (let i = 0; i < res.length; i++) {
         info += '<tr>\n' +
-            '<th scope="row">' + res[i]['uid'] + '</th>\n' +
-            '<td>' + res[i]['username'] + '</td>\n' +
-            ' <td>' + res[i]['name'] + '</td>\n' +
-            '<td>' + res[i]['email'] + '</td>\n' +
-            '<td>' + res[i]['phone'] + '</td>\n' +
-            ' <td>' + res[i]['gender'] + '</td>\n' +
-            '<td>' + res[i]['createTime'] + '</td>\n' +
+            '<th scope="row"><a href="javascript:;">' + res[i]['title'] + '</a></th>\n' +
+            '<td><a href="javascript:;">'
+
+        // 调节知识的隐藏长度
+        if (res[i]['text'].length > 5) {
+            info += res[i]['text'].slice(0, 5) + "..."
+        }
+        else {
+            info += res[i]['text']
+        }
+        info += '</a></td>\n' +
             '<td>\n' +
             '<div class="d-flex order-actions">\n' +
-            '<a href="#"><i class="bx bxs-edit"></i></a>\n' +
             '<p>&nbsp;&nbsp;&nbsp;</p>\n' +
-            '<a href="javascript: delete_user(' + res[i]['uid'] + ');"><i class="bx bxs-trash"></i></a>\n' +
+            '<a href="javascript: delete_knowledge(' + res[i]['kid'] + ');"><i class="bx bxs-trash"></i></a>\n' +
             '</div>\n' +
             '</td>\n' +
             '</tr>'
@@ -195,43 +197,12 @@ function addrow() {
     }
 }
 
-//上传新增短语
-function get_tablevalue() {
-    var table = document.getElementById("add_phrases_table");
-    var rows = table.getElementsByTagName("tr");
-
-    for (var i = 0; i < rows.length; i++) {
-        var inputs = rows[i].getElementsByTagName("input");
-        for (var j = 0; j < inputs.length / inputs.length; j++) {
-            var text = inputs[0].value;
-            if (text != null && text != "")
-                upload(text);
-        }
-    }
-
-    alert("添加成功!");
-    window.history.back();
-}
-
-function upload(text) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://1.12.74.230/api/publicphrases");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-            console.log(this.responseText);
-        }
-    });
-    let data = { "content": text };
-    let jsonData = JSON.stringify(data);
-    xhr.send(jsonData);
-}
 // ???????
 //删除用户
-function delete_user(uid) {
+function delete_knowledge(kid) {
 
     var xhr = new XMLHttpRequest();
-    var url = `http://1.12.74.230/api/user/${uid}`;
+    var url = `http://1.12.74.230/api/knowledge/${kid}`;
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
@@ -245,91 +216,24 @@ function delete_user(uid) {
     xhr.send();
 
 
-    // var xhr = new XMLHttpRequest();
-    // xhr.withCredentials = true;
 
-    // xhr.addEventListener("readystatechange", function () {
-    //     if (this.readyState === 4) {
-    //         console.log(this.responseText);
-    //     }
-    // });
-
-    // xhr.open("DELETE", "http://1.12.74.230/api/user/" + uid);
-    // xhr.setRequestHeader("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Nzc3MzY1NTksInVzZXJuYW1lIjoiYWRtaW4ifQ._YnP37NcvX56xu-ZJC6udMs76C7KeYQ9lzjtpFsJ248");
-    // xhr.setRequestHeader("User-Agent", "Apifox/1.0.0 (https://www.apifox.cn)");
-    // xhr.setRequestHeader("Accept", "*/*");
-    // xhr.setRequestHeader("Host", "1.12.74.230");
-    // xhr.setRequestHeader("Connection", "keep-alive");
-
-    // xhr.send();
 
 }
 
-//修改短语
-function handleTableClick(event) {
-    const target = event.target;
-    if (target.tagName.toLowerCase() === 'a') {
-        event.preventDefault();
-        const row = target.parentNode.parentNode.parentNode;
-        const cells = row.querySelectorAll('td');
-        const cell1 = cells[0];
-        const cell2 = cells[2];
 
-        var text1 = cell1.innerHTML;
 
-        cell1.innerHTML = `<input class="form-control" type="text" value=${text1}>`;
-        cell2.innerHTML = '<div class="d-flex order-actions"><button type="button" class="btn btn-info px-3 radius-30" onclick="submitchange(this)">保存</button><p>&nbsp;&nbsp;&nbsp;</p><button type="button" class="btn btn-secondary px-3 radius-30" onclick="cancel(this,`' + text1 + '`)">取消</button></div>';
-    }
-}
-
-//取消修改短语
-function cancel(button, text) {
-    const row = button.parentNode.parentNode.parentNode;
-    const pid = row.querySelectorAll('th')[0];
-    const cells = row.querySelectorAll('td');
-    const cell1 = cells[0];
-    const cell2 = cells[2];
-
-    cell1.innerHTML = text;
-    cell2.innerHTML = '<div class="d-flex order-actions"><a href="#"><i class="bx bxs-edit"></i></a><p>&nbsp;&nbsp;&nbsp;</p><a href="javascript: delete_phrases(' + pid.innerHTML + ');"><i class="bx bxs-trash"></i></a></div>';
-}
-
-//上传修改的短语
-function submitchange(button) {
-    const row = button.parentNode.parentNode.parentNode;
-    const ppid = row.querySelectorAll('th')[0];
-    const cells = row.querySelectorAll('td');
-    const cell = cells[0];
-    const content = cell.querySelectorAll("input")[0];
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("PUT", "http://1.12.74.230/api/publicphrases");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-            var response = JSON.parse(xhr.response);
-            alert(response.msg);
-            location.reload();
-        }
-    });
-    let data = { "ppid": ppid.innerText, "content": content.value };
-    let jsonData = JSON.stringify(data);
-    console.log(jsonData)
-    xhr.send(jsonData);
-}
-
-//搜索用户（uid）
-function user_handleKeyPress(event) {
+//搜索知识（title）
+function knowledge_handleKeyPress(event) {
     if (event.keyCode === 13) {
-        var inputContent = document.getElementById("user_search").value;
+        var inputContent = document.getElementById("knowledge_search").value;
 
-        var Uid = parseInt(inputContent);
+        var kid = parseInt(inputContent);
         var url = "";
 
         if (/^\d+$/.test(inputContent))
-            url = `http://1.12.74.230/api/user/${Uid}`;
+            url = `http://1.12.74.230//api/knowledge/${kid}`;
         else
-            url = `http://1.12.74.230/api/user/page/1/5`;
+            url = `http://1.12.74.230/api/knowledge/page/1/5`;
 
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
@@ -348,13 +252,13 @@ function user_handleKeyPress(event) {
                 //获取单个返回数据
                 else if (response.data != null && response.data.records == null) {
                     response = Array.from([response.data]);
-                    init_user(response);
+                    init_knowledge(response);
                     document.getElementById("pagelist").innerHTML = "";
                 }
 
                 //获取到多个返回数据
                 else if (response.data != null) {
-                    init_user(response.data.records);
+                    init_knowledge(response.data.records);
                     document.getElementById("pagelist").innerHTML = "";
                 }
                 else
